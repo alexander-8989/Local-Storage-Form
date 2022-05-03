606b95045da92c38fd666f5c4e9ef99e457ba19d
alexander-8989 <alexander.m@appwrk.com>
2022-05-03 19:43:54 +0530

    local storage work

--- form.js
+++ form.js
@@ -3,63 +3,79 @@ function myValidate() {
     let firstName = document.getElementById('firstName').value;
     let lastName = document.getElementById('lastName').value;
     let email = document.getElementById('email').value;
-    var gender = document.querySelector('input[name="age"]:checked');
+    var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     let jobTitle = document.getElementById('jobTitle').value;
+    let gender = document.querySelector('input[name="age"]:checked');
     let phone = document.getElementById('phone').value;
+    let phone1 = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
-
-    if (firstName == "") {
+    if (firstName === "") {
         document.getElementById('fname').innerHTML = "please enter first name";
         return false;
     } else if (firstName !== "") {
         document.getElementById('fname').innerHTML = '';
     }
 
-    if (lastName == "") {
+    if (lastName === "") {
         document.getElementById('lname').innerHTML = "please enter last name";
         return false;
     } else if (lastName !== "") {
         document.getElementById('lname').innerHTML = '';
     }
 
-    if (email == "") {
+    if (email === "") {
         document.getElementById('em').innerHTML = "please enter email";
         return false;
     } else if (email !== "") {
         document.getElementById('em').innerHTML = '';
+
     }
 
-    if (gender == null) {
-        document.getElementById('gen').innerHTML = "please select gender";
-        return false;
-    }
-
-    if (jobTitle == "") {
+    if (jobTitle === "") {
         document.getElementById('jobT').innerHTML = "please enter job title";
         return false;
     } else if (jobTitle !== "") {
         document.getElementById('jobT').innerHTML = '';
     }
 
-    if (phone == "") {
+    if (gender === null) {
+        document.getElementById('gen').innerHTML = "please select gender";
+        return false;
+    } else if (gender !== null) {
+        document.getElementById('gen').innerHTML = '';
+    }
+
+    if (phone === "") {
         document.getElementById('phn').innerHTML = "please enter phone number";
         return false;
     } else if (phone !== "") {
         document.getElementById('phn').innerHTML = '';
     }
 
+    /* Local storage validation */
+    var existingEntries = JSON.parse(localStorage.getItem('person'));
+    if (existingEntries == null) {
+        existingEntries = [];
+    }
 
-    /* Local storage vaalidation */
-    localStorage.setItem("firstName", firstName.value);
-    localStorage.setItem("lastName", lastName.value);
-    localStorage.setItem("email", email.value);
-    localStorage.setItem("jobTitle", jobTitle.value);
-    localStorage.setItem("gender", gender.value);
-    localStorage.setItem("phone", phone.value);
+    let person = {
+        'firstname': firstName.trim(),
+        'lastname': lastName.trim(),
+        'email': email,
+        'jobtitle': jobTitle,
+        'gender': gender.value,
+        'phone': phone,
+    };
+    localStorage.setItem('person', JSON.stringify(person));
+    existingEntries.unshift(person);
 
+    localStorage.setItem('person', JSON.stringify(existingEntries));
 
     /* validation for redirect the page */
     window.location.href = "/table.html";
+
+    /* validation for reset the page */
+    document.getElementById("reset").click();
 }
 
 
